1129f3034f1ac85f50b30240d300300eff3b2df7
Cedric <cedmanchau@gmail.com>
2024-09-15 08:12:52 -0700

    d

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,6 @@ export const runtime = 'edge';
 
 export default async function ProductsPage() {
 
-
-
-
-  
   const companiesData = await prepareData(`https://i0yko8ncze.execute-api.us-east-2.amazonaws.com/Prod/Company/List`);
   const companiesNews = await fetchNews();
 
