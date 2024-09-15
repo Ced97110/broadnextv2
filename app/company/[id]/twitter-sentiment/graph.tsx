66c61c2592ea7dabfba1e7e5cf52677f62a72f14
Cedric <cedmanchau@gmail.com>
2024-09-14 23:18:27 -0700

    s

--- app/company/[id]/twitter-sentiment/graph.tsx
+++ app/company/[id]/twitter-sentiment/graph.tsx
@@ -237,14 +237,7 @@ const TwitterSentiment = ({id, period, dataEntities, positiveEntitiesData, negat
                     </BarChart>
                   </ChartContainer>
                 </CardContent>
-                <CardFooter className="flex-col items-start gap-2 text-sm">
-                  <div className="flex gap-2 font-medium leading-none">
-                    Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-                  </div>
-                  <div className="leading-none text-muted-foreground">
-                    Showing total visitors for the last 6 months
-                  </div>
-                </CardFooter>
+            
               </>
             )}
           </Card>
@@ -280,14 +273,7 @@ const TwitterSentiment = ({id, period, dataEntities, positiveEntitiesData, negat
                     </BarChart>
                   </ChartContainer>
                 </CardContent>
-                <CardFooter className="flex-col items-start gap-2 text-sm">
-                  <div className="flex gap-2 font-medium leading-none">
-                    Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-                  </div>
-                  <div className="leading-none text-muted-foreground">
-                    Showing total visitors for the last 6 months
-                  </div>
-                </CardFooter>
+             
               </>
             )}
           </Card>
@@ -313,14 +299,7 @@ const TwitterSentiment = ({id, period, dataEntities, positiveEntitiesData, negat
                     </BarChart>
                   </ChartContainer>
                 </CardContent>
-                <CardFooter className="flex-col items-start gap-2 text-sm">
-                  <div className="flex gap-2 font-medium leading-none">
-                    Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-                  </div>
-                  <div className="leading-none text-muted-foreground">
-                    Showing total visitors for the last 6 months
-                  </div>
-                </CardFooter>
+             
          
           </Card>
         </div>
@@ -345,14 +324,7 @@ const TwitterSentiment = ({id, period, dataEntities, positiveEntitiesData, negat
                     </BarChart>
                   </ChartContainer>
                 </CardContent>
-                <CardFooter className="flex-col items-start gap-2 text-sm">
-                  <div className="flex gap-2 font-medium leading-none">
-                    Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-                  </div>
-                  <div className="leading-none text-muted-foreground">
-                    Showing total visitors for the last 6 months
-                  </div>
-                </CardFooter>
+             
          
           </Card>
         </div>
@@ -384,14 +356,7 @@ const TwitterSentiment = ({id, period, dataEntities, positiveEntitiesData, negat
                       </BarChart>
                     </ChartContainer>
                   </CardContent>
-                  <CardFooter className="flex-col items-start gap-2 text-sm">
-                    <div className="flex gap-2 font-medium leading-none">
-                      Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-                    </div>
-                    <div className="leading-none text-muted-foreground">
-                      Showing total visitors for the last 6 months
-                    </div>
-                  </CardFooter>
+               
                 </>
               )}
             </Card>
