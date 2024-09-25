d42e2c0705a3f38e4c38a268ff42295cb973b688
Cedric <cedmanchau@gmail.com>
2024-09-25 11:05:27 -0700

    s

--- app/(dashboard)/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from 'lucide-react'
-import React from 'react'
-export default function Loading() {
-  return (
-    <div className='flex items-center justify-center h-1/2'>
-      <Loader className="animate-spin text-muted-foreground h-10 w-10" />
-    </div>
-  )
-}
