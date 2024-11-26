0ea692d8d32f0a43aa01ee5fe5bbd75a72bd5a7c
Cedric <cedmanchau@gmail.com>
2024-11-25 21:28:23 -0400

    s

--- app/api/auth/[...auth0]/route.ts
+++ app/api/auth/[...auth0]/route.ts
@@ -4,8 +4,8 @@ export const GET = handleAuth({
     login: handleLogin({
         returnTo: '/dashboard',
         authorizationParams: {
-            audience: 'https://auth0-authorizer1',
-            issuerBaseURL: 'https://dev-0g677pmhrof4rua3.us.auth0.com/',
+            audience: 'https://dev-0g677pmhrof4rua3.us.auth0.com/api/v2/',
+            
         }
     }),
  })
