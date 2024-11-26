ec0f8625725a5292b11ecce4a29c09f10d0757cc
Cedric <cedmanchau@gmail.com>
2024-11-26 08:17:10 -0400

    s

--- app/api/auth/[...auth0]/route.ts
+++ app/api/auth/[...auth0]/route.ts
@@ -1,4 +1,12 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth()
-
+export const GET = handleAuth({
+    login: handleLogin({
+        returnTo: '/dashboard',
+        authorizationParams: {
+            audience: 'https://dev-0g677pmhrof4rua3.us.auth0.com/api/v2/',
+            
+            
+        }
+    }),
+ })
