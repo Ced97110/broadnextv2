00944b0e8f1df2dea7bad11e40232811f071ff16
Cedric <cedmanchau@gmail.com>
2024-11-28 12:46:20 -0400

    s

--- app/api/auth/[...auth0]/route.ts
+++ app/api/auth/[...auth0]/route.ts
@@ -1,12 +1,26 @@
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import { handleAuth, handleCallback, handleLogin } from '@auth0/nextjs-auth0/edge';
+import { redirect } from 'next/navigation';
+
+
+const afterCallback = async (req, session, state) => {
+  console.log("Session: ", session.accessToken);
+        await fetch("https://ajstjomnph.execute-api.us-east-2.amazonaws.com/Prod/usermanagement/SaveUser",{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${session.accessToken}`
+        },
+      });
+      return session;
+}
+
 
 export const GET = handleAuth({
     login: handleLogin({
         returnTo: '/dashboard',
         authorizationParams: {
             audience: 'https://auth0-authorizer1',
-            
-            
         }
     }),
+    callback: handleCallback({ afterCallback })
  })
