eb4ac2426aebfa730296e01226afadd3c1ec4e67
Cedric <cedmanchau@gmail.com>
2024-12-07 09:08:47 -0800

    s

--- app/(dashboard)/dashboard/companies/companies-table.tsx
+++ app/(dashboard)/dashboard/companies/companies-table.tsx
@@ -14,7 +14,7 @@ import {
   import Image from 'next/image'
 import Link from 'next/link'
 import { useUser } from '@auth0/nextjs-auth0/client'
-import { CheckCircle, CircleMinus, CirclePlus } from 'lucide-react'
+import { Briefcase, CheckCircle, CircleMinus, CirclePlus, DollarSign, Factory, Globe, Search } from 'lucide-react'
 import { Spinner } from '@/components/icons'
 import { CompanyUser, handleInterested, handleRemove, handleWatchList } from '@/lib/data'
 import { Button } from '@/components/ui/button'
@@ -109,164 +109,202 @@ import { Input } from '@/components/ui/input'
    
 
     return (
-      <section>
-        
-           <div className='mt-24'>
-              <h2 className="text-2xl font-semibold pb-12">Companies</h2>
-            </div>
-            <div className='flex gap-4 my-7'>
-            <div className='flex-1'>
-              <Input type="search" placeholder="Search" />
-            </div>
-            <div>
-              <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Theme" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                  <SelectItem value="dark">Dark</SelectItem>
-                  <SelectItem value="system">System</SelectItem>
-              </SelectContent>
-            </Select>
-            </div>
-            <div>
-              <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Theme" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                  <SelectItem value="dark">Dark</SelectItem>
-                  <SelectItem value="system">System</SelectItem>
-              </SelectContent>
-            </Select>
-            </div>
-            <div>
-              <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Theme" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                  <SelectItem value="dark">Dark</SelectItem>
-                  <SelectItem value="system">System</SelectItem>
-              </SelectContent>
-            </Select>
-            </div>
-            <div>
-              <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Theme" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                  <SelectItem value="dark">Dark</SelectItem>
-                  <SelectItem value="system">System</SelectItem>
-              </SelectContent>
-            </Select>
-            </div>
-            </div>
-          
-            <Card className="shadow-2xl rounded-xl overflow-hidden w-full">
-                  
-              <Table className="shadow-xl rounded-xl overflow-hidden p-8">
-                <TableHeader className="bg-gray-200">
-                  <TableRow className="bg-gray-200 rounded-t-xl p-4">
-                    <TableHead></TableHead>
-                    <TableHead>Name</TableHead>
-                    <TableHead>Ticker</TableHead>
-                    <TableHead>Sector</TableHead>
-                    <TableHead>Location</TableHead>
-                    <TableHead>Company Type</TableHead>
-                    <TableHead>Price</TableHead>
-                    <TableHead>24h Movement</TableHead>
-                    <TableHead>Market Cap</TableHead>
-                    <TableHead></TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody className='p-4'>
-                  {company?.map(({ 
-                    Id, 
-                    Name, 
-                    LogoUrl,
-                    Sector,
-                    Ticker,
-                    Location,
-                    Type,
-                    ClosePrice,
-                    PriceMovement,
-                    PriceChange,
-                    MarketCap 
-                  },i) => (
-                    <TableRow key={Id}>
-                      {/* Logo */}
-                      <TableCell className="font-medium flex justify-around items-center">
-                        <p>{i+1}</p>
-                        <Link href={`company/${Id}/summary`}>
-                          <Image
-                            src={LogoUrl}
-                            alt={Name}
-                            width={50}
-                            height={50}
-                            className="object-contain aspect-square"
-                          />
-                        </Link>
-                      </TableCell>
-
-                      {/* Name */}
-                      <TableCell>
-                        <Link href={`company/${Id}/summary`}>
-                          {Name ?? 'N/A'}
-                        </Link>
-                      </TableCell>
-
-                      {/* Ticker */}
-                      <TableCell>
-                        <span className="text-xs text-gray-600">{Ticker ?? 'N/A'}</span>
-                      </TableCell>
-
-                      {/* Sector */}
-                      <TableCell>{Sector.charAt(0).toUpperCase() + Sector.slice(1).toLowerCase() ?? 'N/A'}</TableCell>
-
-                      {/* Location */}
-                      <TableCell>{Location.charAt(0).toUpperCase() + Location.slice(1).toLowerCase() ?? 'N/A'}</TableCell>
-
-                      {/* Company Type */}
-                      <TableCell>{Type ?? 'N/A'}</TableCell>
-
-                      {/* Price */}
-                      <TableCell>
-                        {ClosePrice != null ? `$${ClosePrice.toFixed(2)}` : 'N/A'}
-                      </TableCell>
-
-                      {/* 24h Movement */}
-                      <TableCell className="text-center">
-                        {PriceMovement != null && PriceChange != null 
-                          ? <PriceIndicator PriceMovement={Number(PriceMovement)} PriceChange={PriceChange}/> 
-                          : 'N/A'}
-                      </TableCell>
-
-                      {/* Market Cap */}
-                      <TableCell>
-                        {MarketCap ? FormatMarketCap(MarketCap) : 'N/A'}
-                      </TableCell>
-
-                      <TableCell>
-                        <Button variant="ghost">
-                          <Link href={`company/${Id}/summary`}>
-                            <ChevronRight className="w-4 h-4" />
-                          </Link>
-                        </Button>
-                      </TableCell>
-
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Card>
-      
-       
-  </section>
+      <section className='mt-24'>
+      <h2 className="text-2xl font-semibold pb-12">Companies</h2>
+      <div className='flex gap-4 my-4 relative'>
+        {/* Search Input */}
+        <div className='flex-1 relative'>
+          <Input type="search" placeholder="Search" />
+          <span className='absolute right-3 top-1/2 -translate-y-1/2'>
+            <Search className='w-4 h-4 text-gray-500' />
+          </span>
+        </div>
+    
+        {/* Sector Select */}
+        <div>
+          <Select>
+            <SelectTrigger className="w-[180px] flex items-center">
+              <Briefcase className="mr-2 h-4 w-4 text-gray-500" />
+              <SelectValue placeholder="Sector" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="technology">
+                <Briefcase className="mr-2 h-4 w-4 text-gray-500" />
+                Technology
+              </SelectItem>
+              <SelectItem value="finance">
+                <Briefcase className="mr-2 h-4 w-4 text-gray-500" />
+                Finance
+              </SelectItem>
+              <SelectItem value="healthcare">
+                <Briefcase className="mr-2 h-4 w-4 text-gray-500" />
+                Healthcare
+              </SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+    
+        {/* Location Select */}
+        <div>
+          <Select>
+            <SelectTrigger className="w-[180px] flex items-center">
+              <Globe className="mr-2 h-4 w-4 text-gray-500" />
+              <SelectValue placeholder="Location" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="us">
+                <Globe className="mr-2 h-4 w-4 text-gray-500" />
+                United States
+              </SelectItem>
+              <SelectItem value="uk">
+                <Globe className="mr-2 h-4 w-4 text-gray-500" />
+                United Kingdom
+              </SelectItem>
+              <SelectItem value="de">
+                <Globe className="mr-2 h-4 w-4 text-gray-500" />
+                Germany
+              </SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+    
+        {/* Company Type Select */}
+        <div>
+          <Select>
+            <SelectTrigger className="w-[180px] flex items-center">
+              <Factory className="mr-2 h-4 w-4 text-gray-500" />
+              <SelectValue placeholder="Company Type" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="public">
+                <Factory className="mr-2 h-4 w-4 text-gray-500" />
+                Public
+              </SelectItem>
+              <SelectItem value="private">
+                <Factory className="mr-2 h-4 w-4 text-gray-500" />
+                Private
+              </SelectItem>
+              <SelectItem value="startup">
+                <Factory className="mr-2 h-4 w-4 text-gray-500" />
+                Startup
+              </SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+    
+        {/* Market Cap Select */}
+        <div>
+          <Select>
+            <SelectTrigger className="w-[180px] flex items-center">
+              <DollarSign className="mr-2 h-4 w-4 text-gray-500" />
+              <SelectValue placeholder="Market Cap" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="large">
+                <DollarSign className="mr-2 h-4 w-4 text-gray-500" />
+                Large Cap
+              </SelectItem>
+              <SelectItem value="mid">
+                <DollarSign className="mr-2 h-4 w-4 text-gray-500" />
+                Mid Cap
+              </SelectItem>
+              <SelectItem value="small">
+                <DollarSign className="mr-2 h-4 w-4 text-gray-500" />
+                Small Cap
+              </SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+    
+      {/* Table Section */}
+      <Card className="shadow-2xl rounded-xl overflow-hidden w-full">
+        <Table className="shadow-xl rounded-xl overflow-hidden p-8">
+          <TableHeader className="bg-gray-200">
+            <TableRow className="bg-gray-200 rounded-t-xl p-4">
+              <TableHead></TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Ticker</TableHead>
+              <TableHead>Sector</TableHead>
+              <TableHead>Location</TableHead>
+              <TableHead>Company Type</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead>24h Movement</TableHead>
+              <TableHead>Market Cap</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody className='p-4'>
+            {company?.map(({ 
+              Id, 
+              Name, 
+              LogoUrl,
+              Sector,
+              Ticker,
+              Location,
+              Type,
+              ClosePrice,
+              PriceMovement,
+              PriceChange,
+              MarketCap 
+            },i) => (
+              <TableRow key={Id}>
+                <TableCell className="font-medium flex justify-around items-center">
+                  <p>{i+1}</p>
+                  <Link href={`company/${Id}/summary`}>
+                    <Image
+                      src={LogoUrl}
+                      alt={Name}
+                      width={50}
+                      height={50}
+                      className="object-contain aspect-square"
+                    />
+                  </Link>
+                </TableCell>
+    
+                <TableCell>
+                  <Link href={`company/${Id}/summary`}>
+                    {Name ?? 'N/A'}
+                  </Link>
+                </TableCell>
+    
+                <TableCell>
+                  <span className="text-xs text-gray-600">{Ticker ?? 'N/A'}</span>
+                </TableCell>
+    
+                <TableCell>{Sector ? Sector.charAt(0).toUpperCase() + Sector.slice(1).toLowerCase() : 'N/A'}</TableCell>
+    
+                <TableCell>{Location ? Location.charAt(0).toUpperCase() + Location.slice(1).toLowerCase() : 'N/A'}</TableCell>
+    
+                <TableCell>{Type ?? 'N/A'}</TableCell>
+    
+                <TableCell>
+                  {ClosePrice != null ? `$${ClosePrice.toFixed(2)}` : 'N/A'}
+                </TableCell>
+    
+                <TableCell className="text-center">
+                  {PriceMovement != null && PriceChange != null 
+                    ? <PriceIndicator PriceMovement={Number(PriceMovement)} PriceChange={PriceChange}/> 
+                    : 'N/A'}
+                </TableCell>
+    
+                <TableCell>
+                  {MarketCap ? FormatMarketCap(MarketCap) : 'N/A'}
+                </TableCell>
+    
+                <TableCell>
+                  <Button variant="ghost">
+                    <Link href={`company/${Id}/summary`}>
+                      <ChevronRight className="w-4 h-4" />
+                    </Link>
+                  </Button>
+                </TableCell>
+    
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Card>
+    </section>
     )
   }
