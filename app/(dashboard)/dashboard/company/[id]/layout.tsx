d688c03221b320cb2f3fc49d4689adec457f7dda
Cedric <cedmanchau@gmail.com>
2024-12-09 06:15:21 -0800

    z

--- app/(dashboard)/dashboard/company/[id]/layout.tsx
+++ app/(dashboard)/dashboard/company/[id]/layout.tsx
@@ -10,6 +10,7 @@ import ImageLoading from "./Image-loading";
 import Script from 'next/script';
 import PriceIndicator from "../price-indicator";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { getSession } from "@auth0/nextjs-auth0/edge";
 
 
 export interface CompanyRelation {
@@ -43,6 +44,8 @@ export default async function DashboardLayout({
   }) {
 
    
+  const { user } = await getSession();
+  console.log('user', user)
 
     const companyRelation = await CompanyFetch(params.id) as CompanyRelation;
     const companyData = companyRelation.Company;
@@ -98,11 +101,18 @@ export default async function DashboardLayout({
           </div>
         </div>
       </Providers>
-         <Script type="text/javascript">
-         {`window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","identifyHashed","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
-           heap.load("1884442793")
-          `}
-       </Script>
+      <Script type="text/javascript">
+        {`window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","identifyHashed","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
+          heap.load("1884442793")
+          heap.identify("${user?.name}")
+           heap.addUserProperties({
+            email: "${user?.email}",
+            name: "${user?.name}",
+            picture: "${user?.picture}",
+            email_verified: "${user?.email_verified}",
+            sub: "${user?.sub}"
+           })`}
+      </Script>
        </>
     )
   }
