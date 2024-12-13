c135677ed3a18e4e70dbc773d2ea01c50039942e
Cedric <cedmanchau@gmail.com>
2024-12-13 12:42:39 -0800

    s

--- app/(dashboard)/dashboard/companies/portfolio.tsx
+++ app/(dashboard)/dashboard/companies/portfolio.tsx
@@ -2,9 +2,10 @@
 
 import React from 'react';
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 import { CiCircleCheck } from "react-icons/ci";
 import Loading from '../../load';
+import { FaCheckCircle } from 'react-icons/fa';
 
 interface PortfolioIconComponentProps {
   Id: number;
@@ -17,31 +18,25 @@ interface PortfolioIconComponentProps {
 
 export default function Portfolio ({Id,InPortfolio,handleRemovePortfolio,handleAddPortfolio,loading }:PortfolioIconComponentProps ) {
 
-  const handleClick = () => {
-    if (InPortfolio) {
-      handleRemovePortfolio(Id);
-    } else {
-      handleAddPortfolio(Id);
-    }
-  };
   return (
-    <Button onClick={handleClick} className="rounded-full cursor-pointer" variant="outline">
-      {loading ?  <Loading/> : InPortfolio ? (
-        <>
-        <CiCircleCheck
-          className={`cursor-pointer h-4 w-4 star active`}
-          aria-label="Retirer de la watchlist"
-          
-        />
-         In My Portfolio
-        </>
-      ) : (
-        <>
+    <>
+    {loading ? <Button className="rounded-full cursor-pointer" disabled>
+      <Loader2 className="animate-spin" />
+        Please wait
+     </Button> :
+     !InPortfolio ? (
+      <Button onClick={() => handleAddPortfolio(Id)} className="rounded-full cursor-pointer" variant="outline">
           <Plus className="mr-2 h-4 w-4" />
-          Add to my portfolio
-        </>
-      )}
-    </Button>
+           Add to My Portfolio
+       </Button> 
+    ) : (
+      <Button onClick={() => handleRemovePortfolio(Id)} className="rounded-full cursor-pointer" variant="outline">
+        <FaCheckCircle className="mr-2 h-4 w-4" />
+          In My portfolio
+     </Button> 
+    )}
+    </>
+   
   );
 };
 
