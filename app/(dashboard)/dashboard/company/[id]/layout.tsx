0be8156e0640378e3f6713e2ff50c1238cc81d5f
Cedric <cedmanchau@gmail.com>
2024-12-13 13:23:54 -0800

    s

--- app/(dashboard)/dashboard/company/[id]/layout.tsx
+++ app/(dashboard)/dashboard/company/[id]/layout.tsx
@@ -13,6 +13,7 @@ import Link from "next/link";
 import { InteractiveLayoutBadges } from "../interactive-layout";
 import { getSession } from "@auth0/nextjs-auth0/edge";
 import { redirect } from "next/navigation";
+import Script from "next/script";
 
 
 export interface CompanyRelation {
@@ -116,6 +117,15 @@ export default async function DashboardLayout({
           </div>
         </div>
       </Providers>
+      <Script type="text/javascript" strategy="lazyOnload">
+        {`window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","identifyHashed","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
+          heap.load("1884442793")
+          heap.identify("${user?.sub}")
+           heap.addUserProperties({
+            Name: "${user?.name}",
+            Email: "${user?.email}",
+           })`}
+      </Script>
        </>
     )
   }
