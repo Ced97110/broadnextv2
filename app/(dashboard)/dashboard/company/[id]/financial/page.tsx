823e8713c4dd5a9a3dcaa668a5c2dd9530285cfb
Cedric <cedmanchau@gmail.com>
2024-12-15 22:16:17 -0800

    x

--- app/(dashboard)/dashboard/company/[id]/financial/page.tsx
+++ app/(dashboard)/dashboard/company/[id]/financial/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { DataFetch } from '@/lib/data';
 import Financials from './financial';
 
+export const runtime = 'edge';
+
 export default async function FinancialsWrapper({ params }: { params: { id: string } }) {
   const result = await DataFetch(params.id);
   return (
