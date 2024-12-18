a0d345802d5e97dc5cb8e5f71bc222db40a7a6e6
Cedric <cedmanchau@gmail.com>
2024-12-17 20:56:03 -0800

    w

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -2,7 +2,22 @@
 import { CompanyUser } from "@/lib/data";
 import UserSelection from "./user-selection";
 
+export const revalidate = 300;
 
+
+export async function generateStaticParams() {
+  const response = await fetch(`https://ajstjomnph.execute-api.us-east-2.amazonaws.com/Prod/usermanagement/ListCompanies`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = await response.json();
+  return data.map((item) => ({
+    Id: String(item.Id)
+   }));
+   
+ }
 
 export default async function HomePage() {
 
